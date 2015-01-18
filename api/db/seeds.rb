451509bf1d69e0785f78f4239c947300a97b7475
Antonio <toony1908@gmail.com>
2015-01-18 07:14:41 -0500

    def

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+ status = Status.create([{status: 'Everything is ogoing ok'}])
--- pebble/coordenadas.js
+++ pebble/coordenadas.js
@@ -1,19 +1,10 @@
-/**
- * Welcome to Pebble.js!
- *
- * This is where you write your app.
- */
 var UI = require('ui');
+var ajax = require('ajax');
 var card = new UI.Card({
 });
 card.show();
 navigator.geolocation.getCurrentPosition(function(pos) {
   var coords = pos.coords;
-  //var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?' +
-      //'lat=' + coords.latitude + '&lon=' + coords.longitude + '&units=metric';
-  
-    var ajax = require('ajax');
-
 ajax(
   {
     url: 'https://morning-brook-3318.herokuapp.com/status/',
@@ -29,7 +20,7 @@ ajax(
   },
   function(data) {
 
-    card.title( 'Done');
+    card.title( 'Danger');
   },
   function(error) {
     console.log( error);
@@ -39,6 +30,97 @@ ajax(
   
   
   });
+card.on('click', 'up', function() {
+  navigator.geolocation.getCurrentPosition(function(pos) {
+  var coords = pos.coords;
+  //var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?' +
+      //'lat=' + coords.latitude + '&lon=' + coords.longitude + '&units=metric';
+  
+    
 
+ajax(
+  {
+    url: 'https://morning-brook-3318.herokuapp.com/status/',
+    type: 'json',
+    method: 'post',
+    data: {
+      status: {
+        latitude: coords.latitude,
+        longitude: coords.longitude,
+        status: 'Im ok'
+  }
+    }
+  },
+  function(data) {
+
+    card.title( 'Im ok');
+  },
+  function(error) {
+    console.log( error);
+  }
+);
+ 
+  
+  
+  });
+});
+
+card.on('click', 'down', function() {
+   
+  ajax(
+  {
+    url: 'https://morning-brook-3318.herokuapp.com/status/',
+    type: 'json',
+    method: 'post',
+    data: {
+      status: {
+        latitude: '0',
+        longitude: '0',
+        status: 'made a mistake'
+  }
+    }
+  },
+  function(data) {
+
+    card.title( 'made a mistake');
+  }
+);
+ 
+});
+
+card.on('click', 'select', function() {
+  navigator.geolocation.getCurrentPosition(function(pos) {
+  var coords = pos.coords;
+  //var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?' +
+      //'lat=' + coords.latitude + '&lon=' + coords.longitude + '&units=metric';
+  
+    
+
+ajax(
+  {
+    url: 'https://morning-brook-3318.herokuapp.com/status/',
+    type: 'json',
+    method: 'post',
+    data: {
+      status: {
+        latitude: coords.latitude,
+        longitude: coords.longitude,
+        status: 'please help me!!!'
+  }
+    }
+  },
+  function(data) {
+
+    card.title( 'Need some help');
+  },
+  function(error) {
+    console.log( error);
+  }
+);
+ 
+  
+  
+  });
+});
 
 
