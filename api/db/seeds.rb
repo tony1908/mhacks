5a92fa450894b65f7a8224a5ac260c047ccb0837
Antonio <toony1908@gmail.com>
2015-01-18 09:32:32 -0500

    more seed

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
- status = Status.create([{status: 'Everything is ogoing ok'}])
+	status = Status.create([{status: 'Everything is ogoing ok', latitude: '1i', longitude: '2i'}])
